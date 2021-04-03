a5971248687b86f419cdaf22cc4096777bea2c33
David Strube <dstrube@gmail.com>
2021-04-03 08:40:27 -0400

    Minor edits

--- CustomDialog0/app/src/main/java/com/dstrube/customdialog/MainActivity.java
+++ CustomDialog0/app/src/main/java/com/dstrube/customdialog/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
