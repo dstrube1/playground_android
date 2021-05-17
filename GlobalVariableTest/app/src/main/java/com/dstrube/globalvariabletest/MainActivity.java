411988fe285909c72a2f6fdb826bc9b68c5858aa
David Strube <dstrube3@gatech.edu>
2021-05-16 22:26:26 -0400

    Minor edits

--- GlobalVariableTest/app/src/main/java/com/dstrube/globalvariabletest/MainActivity.java
+++ GlobalVariableTest/app/src/main/java/com/dstrube/globalvariabletest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.dstrube.globalvariabletest;
 
-import android.app.Application;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
