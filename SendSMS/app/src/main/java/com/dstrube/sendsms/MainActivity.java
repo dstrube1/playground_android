6eb67684ebf0fa0606a91c6ea3d811d9ac3cbd10
David Strube <dstrube3@gatech.edu>
2021-06-25 07:56:00 -0400

    minor edits

--- SendSMS/app/src/main/java/com/dstrube/sendsms/MainActivity.java
+++ SendSMS/app/src/main/java/com/dstrube/sendsms/MainActivity.java
@@ -1,8 +1,8 @@
 package com.dstrube.sendsms;
 
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
