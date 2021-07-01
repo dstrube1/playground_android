8404d877ef232e4e9fcfd01161b0f51f62bb6b2d
David Strube <dstrube3@gatech.edu>
2021-07-01 00:17:53 -0400

    minor edits

--- SimpleNotification/app/src/main/java/com/dstrube/simplenotification/MainActivity.java
+++ SimpleNotification/app/src/main/java/com/dstrube/simplenotification/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
