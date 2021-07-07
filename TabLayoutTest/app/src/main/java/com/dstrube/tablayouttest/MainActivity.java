d5b727b9a443371e9f476d3fe2b45ca21a620536
David Strube <dstrube3@gatech.edu>
2021-07-07 10:57:24 -0400

    minor edits

--- TabLayoutTest/app/src/main/java/com/dstrube/tablayouttest/MainActivity.java
+++ TabLayoutTest/app/src/main/java/com/dstrube/tablayouttest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dstrube.tablayouttest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
@@ -21,20 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Tab for Photos
         TabSpec photospec = tabHost.newTabSpec("Photos");
-        photospec.setIndicator("Photos", getDrawable(R.drawable.icon_photos_tab));
+        photospec.setIndicator("Photos", AppCompatResources.getDrawable(this,R.drawable.icon_photos_tab));
         Intent photosIntent = new Intent(this, PhotosActivity.class);
         photospec.setContent(photosIntent);
 
         // Tab for Songs
         TabSpec songspec = tabHost.newTabSpec("Songs");
         // setting Title and Icon for the Tab
-        songspec.setIndicator("Songs", getDrawable(R.drawable.icon_songs_tab));
+        songspec.setIndicator("Songs", AppCompatResources.getDrawable(this,R.drawable.icon_songs_tab));
         Intent songsIntent = new Intent(this, SongsActivity.class);
         songspec.setContent(songsIntent);
 
         // Tab for Videos
         TabHost.TabSpec videospec = tabHost.newTabSpec("Videos");
-        videospec.setIndicator("Videos", getDrawable(R.drawable.icon_videos_tab));
+        videospec.setIndicator("Videos", AppCompatResources.getDrawable(this,R.drawable.icon_videos_tab));
         Intent videosIntent = new Intent(this, VideosActivity.class);
         videospec.setContent(videosIntent);
 
