f5d300593320f6a1dc93c1e31945a65a48c55311
David Strube <dstrube3@gatech.edu>
2021-08-28 22:12:24 -0400

    Placeholder to move broken code from fix folder to here

---
+++ ListView2/app/src/main/java/com/dstrube/listview2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dstrube.listview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Placeholder to move broken code from fix folder to here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
