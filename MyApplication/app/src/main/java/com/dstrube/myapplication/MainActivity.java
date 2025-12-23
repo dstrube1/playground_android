95578a17465d21652084f40ed81b7509273daf97
David Strube <dstrube3@gatech.edu>
2025-12-23 10:23:49 -0500

    Minor edis

--- MyApplication/app/src/main/java/com/dstrube/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/dstrube/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package com.dstrube.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 import com.dstrube.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = MainActivity.class.getName();
 
     // Used to load the 'myapplication' library on application startup.
     static {
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
-
+        //Button button = binding.button;
         /*
         OR:
         super.onCreate(savedInstanceState);
@@ -39,10 +44,16 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(stringFromJNI());
          */
     }
+    public void handleClick(View view) {
+        Log.d(TAG, "before binaryTreeSort");
+        MySortTest.binaryTreeSort(0);
+        Log.d(TAG, "after binaryTreeSort");
+    }
 
     /**
      * A native method that is implemented by the 'myapplication' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
 }
